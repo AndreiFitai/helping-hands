d1d80a027551c487514e9c072d227d24b641de54
Nina Drüke <ndrueke@posteo.de>
2018-09-03 13:51:39 +0200

    nothing changed -.-

--- models/Event.js
+++ models/Event.js
@@ -6,13 +6,13 @@ const userSchema = new Schema({
   poc: String,
   address: String,
   description: String,
-  date: Date
-  pictures:[String]
+  date: Date,
+  pictures:[String],
   needs:{
     ammount_of_ppl: Number,
     need_desc: String,
     car: Boolean,
-  }
+  },
   location: {
     type: {
       type: String
@@ -20,11 +20,11 @@ const userSchema = new Schema({
     coordinates: [Number]
   },
   participants: [{
-    userId: String,
+    userId:
     { type: String,
       enum: ["Organizer", "Volunteer"],
       default: "Volunteer"}
-  }]
+  }],
   reported: {
     reporter_id: String,
     message: String,
