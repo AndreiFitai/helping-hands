59326936e74b84f8f51437bcd9b6fe212fd425b5
Nina Drüke <ndrueke@posteo.de>
2018-09-06 18:32:33 +0200

    good job andrei

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,3 +5,16 @@ document.addEventListener(
   },
   false
 );
+
+Date.prototype.toDateInputValue = (function() {
+  var local = new Date(this);
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+  return local.toJSON().slice(0,10);
+});
+
+$(document).ready( function() {
+  let future = new Date
+  
+  $('#today').val(new Date().toDateInputValue());
+  $("#future").val(new Date(future.setDate(future.getDate()+1)).toDateInputValue());
+});
